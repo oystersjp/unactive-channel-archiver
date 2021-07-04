d1859b15d18ebf49ebdb8c1a8b537050a4f088d9
IkezoeMakoto <150283t@gmail.com>
2021-07-04 20:47:28 +0900

    実装した

--- packages/local/main.go
+++ packages/local/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "archiver/archiver"
+import (
+	"os"
+	"strconv"
+	"archiver/archiver"
+)
 
 func main() {
-	archiver.Say()
+	token := os.Getenv("SLACK_TOKEN")
+	expireDate, _ := strconv.Atoi(os.Getenv("EXPIRE_DATE"))
+	summaryCh := os.Getenv("SUMMARY_CHANNEL")
+
+	service := archiver.NewArchiver(token, []string{}, expireDate)
+	service.Exec(summaryCh)
 }
